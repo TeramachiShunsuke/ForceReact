5e53742df4701b01a41d10e50025cc184e3b6781
TeramachiShunsuke <like.football.17@gmail.com>
2021-06-09 17:03:17 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// Electronの実行に必要なモジュールを取り込む
+const electron = require('electron')
+const path = require('path')
+const url = require('url')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+// Electronのライフサイクルを定義
+let mainWindow // メインウィンドウを表す変数
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+})
+
+// ウィンドウを作成してコンテンツを読み込む
+function createWindow () {
+    mainWindow = new BrowserWindow({width: 800, height: 600,webPreferences: { nodeIntegration: true,contextIsolation: false, }})
+    mainWindow.loadURL(url.format({ 
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    // mainWindow.webContents.openDevTools()
+  // ウィンドウが閉じるときの処理
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+const {clipboard} = require('electron')
+
+// コンポーネントを定義
+export default class App extends Component {
+    render () {
+        return ( <div className='window'>Hello World !</div>)
+    }
+}
+// DOMを書き換え
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'))
